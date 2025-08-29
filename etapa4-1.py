cc18cec70a873f733c1f3e45900c10f95df22fc1
joaolavina <jgslavina@furb.br>
2025-08-28 21:17:09 -0300

    começo etapa 4

---
+++ etapa4-1.py
@@ -0,0 +1,19 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from collections import deque
+
+# Configuração inicial do grid e do grafo
+grid = [
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 3, 1, 1, 1, 1],
+    [1, 1, 2, 2, 1, 3, 3, 2, 1, 1],
+    [1, 1, 2, 1, 3, 3, 3, 2, 1, 1],
+    [1, 1, 2, 2, 3, 3, 3, 2, 2, 1],
+    [1, 1, 1, 2, 3, 1, 2, 2, 1, 1],
+    [1, 1, 1, 1, 2, 3, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, -1, 1, 1, 1, 1]
+]
